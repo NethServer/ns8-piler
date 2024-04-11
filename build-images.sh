98e5d26d90029b5e340567805139acc20c4773f8
Stephane de Labrusse <stephdl@de-labrusse.fr>
2024-04-11 19:18:04 +0200

    Update build-images.sh to use updated versions of MariaDB and Memcached

--- build-images.sh
+++ build-images.sh
@@ -35,7 +35,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=node:fwadm traefik@any:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/sutoj/piler:1.4.4 docker.io/mariadb:10.7.4 docker.io/memcached:1.6.17-alpine" \
+    --label="org.nethserver.images=docker.io/sutoj/piler:1.4.4 docker.io/mariadb:10.11.7 docker.io/memcached:1.6.26-alpine" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
