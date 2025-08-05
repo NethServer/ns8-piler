a0bbe8a2a182eee1dd478f428530c219272093da
Stephane de Labrusse <stephdl@de-labrusse.fr>
2025-08-05 10:15:24 +0200

    Refactor service startup and add Manticore and MariaDB configurations for 1.4.7 (#34)
    NethServer/dev#7516

--- imageroot/templates/config-site.php
+++ imageroot/templates/config-site.php
@@ -3,6 +3,7 @@ define('SITE_NAME_CONST', 'SITE_NAME');
 
 $config[SITE_NAME_CONST] = '{{host}}';
 $config['SITE_URL'] = 'https://{{host}}/';
+$config['PATH_PREFIX'] = '/';
 
 $config['SMTP_DOMAIN'] = $config[SITE_NAME_CONST];
 $config['SMTP_FROMADDR'] = 'no-reply@' . $config[SITE_NAME_CONST];
@@ -17,8 +18,11 @@ $config['DECRYPT_ATTACHMENT_BINARY'] = '/usr/bin/pileraget';
 $config['PILER_BINARY'] = '/usr/sbin/piler';
 $config['DB_HOSTNAME'] = '127.0.0.1';
 $config['DB_PASSWORD'] = 'piler';
-$config['ENABLE_MEMCACHED'] = 1;
-$memcached_server = ['memcached', 11211];
+$config['DB_DATABASE'] = 'piler';
+$config['DB_USERNAME'] = 'piler';
+
+$config['MEMCACHED_ENABLED'] = 1;
+$memcached_server = ['127.0.0.1', 11211];
 
 $config['ENABLE_IMAP_AUTH'] = 0;
 $config['RESTORE_OVER_IMAP'] = 0;
@@ -29,3 +33,10 @@ $config['IMAP_PORT'] =  993;
 $config['IMAP_SSL'] = true;
 
 $config['CAPTCHA_FAILED_LOGIN_COUNT'] = 0;
+
+$config['SPHINX_DRIVER'] = 'sphinx';
+$config['SPHINX_DATABASE'] = '';
+$config['SPHINX_HOSTNAME'] = '127.0.0.1:9306';
+$config['SPHINX_HOSTNAME_READONLY'] = '127.0.0.1:9307';
+$config['SPHINX_MAIN_INDEX'] = 'piler1';
+$config['RT'] = 1;
